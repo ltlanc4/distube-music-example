df70e877c611ac47d94cd5d798bfd811da36fe35
Lê Thành Lân <87648667+ltlanc4@users.noreply.github.com>
2023-04-13 23:33:17 +0700

    Update index.js

--- handler/index.js
+++ handler/index.js
@@ -47,7 +47,7 @@ module.exports = async (client) => {
         //     .commands.set(arrayOfSlashCommands);
 
         // Register for all the guilds the bot is in
-        // await client.application.commands.set(arrayOfSlashCommands);
+        await client.application.commands.set(arrayOfSlashCommands);
     });
 
     // mongoose
